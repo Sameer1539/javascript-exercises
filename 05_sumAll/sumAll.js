682cbdeb547ef1ca51f3f8d3042182b35b52d720
Sameer <118702965+Sameer1539@users.noreply.github.com>
2023-05-16 12:07:26 -0400

    Complete exercise 5 - sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x >= 0 && y >= 0 && typeof x === "number" && typeof y === "number") {
+        let sum = 0;
+        if (x < y){
+            for (let i = x; i <= y; i++) {
+                sum += i;
+            }
+        } else if (x > y) {
+            for (let i = y; i <= x; i++) {
+                sum += i;
+            }
+        } 
+        return sum;
+    } 
+    return "ERROR";
 };
 
 // Do not edit below this line
