eb2b441eff2d0d8dcea08712933506adef9f33ca
Sameer <118702965+Sameer1539@users.noreply.github.com>
2023-05-16 00:33:02 -0400

    Complete exercise 03 - reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // Split the input into an array
+    const splitWord = word.split("");
+    // Create an array to store the same as the variable above but in reverse order
+    let reverse = [];
+    // The for loop starts at the last index of the word and goes back so I create an
+    // index variable that'll be used in the for loop to record the word in reverse.
+    let index = 0;
+    for (let i = splitWord.length - 1; i >= 0; i--) {
+        reverse[index] = splitWord[i];
+        index++;
+    }
+    // Finally, reversed is an array and we want to return it as a string so we use join().
+    reverse = reverse.join("");
+    return reverse;
 
 };
 
