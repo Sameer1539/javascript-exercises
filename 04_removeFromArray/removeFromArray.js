33ca20f6278137238f4168974321cec54becf01d
Sameer <118702965+Sameer1539@users.noreply.github.com>
2023-05-16 11:46:08 -0400

    Complete exercise 4 - removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // For..of loop for each argument after the array
+    for (let itemToRemove of args) {
+        // Loop through the array
+        for (let i = 0; i < arr.length; i++) {
+            // If found, remove the other arguments from the array
+            if (itemToRemove === arr[i]) {
+                arr.splice(i, 1);
+            }
+        }
+    } 
+    return arr;
 
 };
 
