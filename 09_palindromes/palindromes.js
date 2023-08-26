78b95b4124b145e131a3ef917255856850213149
Sameer1539 <118702965+Sameer1539@users.noreply.github.com>
2023-08-26 03:54:12 -0400

    Create a palindrome function that works with single words.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const lastChar = str.length - 1;
+    let reversed = "";
+    for (let i = lastChar; i >= 0; i--){
+        reversed += str[i];
+    }
+    if (reversed = str) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
