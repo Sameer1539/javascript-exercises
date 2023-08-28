8c1fb3e136cc374de9a6d4fb32fdc611edf12d75
Sameer1539 <118702965+Sameer1539@users.noreply.github.com>
2023-08-28 03:05:25 -0400

    Finish palindromes function.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,22 @@
 const palindromes = function (str) {
+    str = str.toLowerCase();
     const lastChar = str.length - 1;
     let reversed = "";
+    let updatedStr = "";
     for (let i = lastChar; i >= 0; i--){
-        let upperCaseIndex = str[i].toUpperCase()
+        let upperCaseIndex = str[i].toUpperCase();
         if (upperCaseIndex >= 'A' && upperCaseIndex <= 'Z') {
             reversed += str[i];
         }
     }
-    if (reversed = str) {
+    for (let i = 0; i <= lastChar; i++) {
+        let upperCaseIndex = str[i].toUpperCase();
+        if (upperCaseIndex >= 'A' && upperCaseIndex <= 'Z')
+        {
+            updatedStr += str[i];
+        }
+    }
+    if (reversed == updatedStr) {
         return true;
     }
     return false;
